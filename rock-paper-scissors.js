7a5ba49500850e9d11f8537063e37afad1b2692d
soupsmagilly <136518804+soupsmagilly@users.noreply.github.com>
2023-08-25 12:58:12 -0500

    Cleaned up the code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,33 +1,46 @@
-//array of options for computer and player to choose
-const ROCK = 'rock';
-const PAPER = 'paper';
-const SCISSORS = 'scissors';
+let playerScore = 0;
+let compScore = 0;
 
-//generates computer's random selction from options
-function getComputerChoice() {
-    let choices = [ROCK, PAPER, SCISSORS];
-    let random = choices[Math.floor(Math.random() * choices.length)];
-    return random;
+const computerPlay = () => {
+    const arrOfChoices = ['rock', 'paper', 'scissors']
+    const randomNum = Math.floor(Math.random() * arrOfChoices.length)
+    return arrOfChoices[randomNum];
 }
 
-//determines the winner between the computer and the player
-function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-           return "It's a tie!";
+const playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return `It\'s a tie, you both picked ${playerSelection}!`; 
     } else if (
-        (playerChoice === ROCK && computerChoice === SCISSORS) ||
-        (playerChoice === PAPER && computerChoice === ROCK) ||
-        (playerChoice === SCISSORS &&  computerChoice === PAPER)) {   
-            return "Hurray, you win!";    
-    } else {
-        return "Sorry, you lose.";
-    }    
+        playerSelection === 'rock' && computerSelection === 'scissors' ||
+        playerSelection === 'paper' && computerSelection === 'rock' ||
+        playerSelection === 'scissors' && computerSelection === 'paper') {
+            playerScore++;
+            return `You win this round, ${playerSelection} beats ${computerSelection}!`;
+    } else if (
+        computerSelection === 'rock' && playerSelection === 'scissors' ||
+        computerSelection === 'paper' && playerSelection === 'rock' ||
+        computerSelection === 'scissors' && playerSelection === 'paper') {
+            compScore++;
+            return `The computer got the best of you, ${computerSelection} beats ${playerSelection}. You lose!`;
+    }
+        
+    
 }
 
-//displays message of who won the round
-let computerChoice = getComputerChoice();
-let playerChoice = prompt("Choose your weapon.");
-console.log("player: " + playerChoice);
-console.log("computer: " + computerChoice);
-console.log(playRound(playerChoice, computerChoice));
+const game = () => {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Choose your weapon', 'Rock, Paper, or Scissors').toLowerCase();
+        const computerSelection = computerPlay(); 
+        console.log('1 ', playRound(playerSelection, computerSelection));
+    }
 
+    if (playerScore > compScore) {
+        return 'Your beat the computer! Way to go!';
+    } else if (playerScore < compScore) {
+        return 'Your got beat by the computer! You need to work on your throws.';
+    } else {
+        return 'You tied with the computer. Not to shabby!';
+    }
+}
+
+console.log(game());
