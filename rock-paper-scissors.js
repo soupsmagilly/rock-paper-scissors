be5290420fc405cf622ecf14cdb1be8bf1a573f8
soupsmagilly <136518804+soupsmagilly@users.noreply.github.com>
2023-08-29 09:45:52 -0500

    Added CSS styling to make game play easier and more attractive

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,9 +4,9 @@ const scissorsBtn = document.querySelector('.scissors');
 const outcomeDiv = document.querySelector('.outcome');
 const playerScoreSpan = document.querySelector('.player-score');
 const compScoreSpan = document.querySelector('.comp-score');
-
-
-
+const p = document.createElement('p');
+const h2 = document.createElement('h2');
+const winnerWinner = document.querySelector('.winnerWinner');
 
 let playerScore = 0;
 let compScore = 0;
@@ -19,9 +19,7 @@ const computerPlay = () => {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-    console.log(playerSelection, computerSelection);
     if (playerSelection === computerSelection) {
-    const p = document.createElement('p');
     p.innerText = `It\'s a tie, you both picked ${playerSelection}!`; 
     outcomeDiv.appendChild(p);
     } else if (
@@ -29,36 +27,35 @@ const playRound = (playerSelection, computerSelection) => {
             playerSelection === 'paper' && computerSelection === 'rock' ||
             playerSelection === 'scissors' && computerSelection === 'paper') {
     playerScore++;
-    const p = document.createElement('p');
     p.innerText = `You win this round, ${playerSelection} beats ${computerSelection}!`;
-    outcomeDiv.appendChild(p);
+    outcomeDiv.appendChild(p); 
     } else if (
             computerSelection === 'rock' && playerSelection === 'scissors' ||
             computerSelection === 'paper' && playerSelection === 'rock' ||
             computerSelection === 'scissors' && playerSelection === 'paper') {
     compScore++;
-    const p = document.createElement('p');
     p.innerText = `The computer got the best of you, ${computerSelection} beats ${playerSelection}. You lose!`;
     outcomeDiv.appendChild(p);
     } 
 }
 
+
 const checkForWinner = (playerScore, compScore) => {
-    console.log(playerScore, compScore)
     if (playerScore === 5) {
         const h2 = document.createElement('h2');
         h2.classList.add('player-won');
         h2.innerText = 'You beat the computer! Way to go!';
-        outcomeDiv.append(h2);
+        winnerWinner.append(h2);
     }
     if (compScore === 5) {
-        const h2 = document.createElement('h2');
+       
         h2.classList.add('comp-won');
         h2.innerText = 'The computer won. You need to work on your throws.';
-        outcomeDiv.append(h2);
+        winnerWinner.append(h2);
     }
 }
- 
+
+
 const updateScores = (playerScore, compScore) => {
     playerScoreSpan.innerText = `Player Score: ${playerScore}`;
     compScoreSpan.innerText = `Computer Score: ${compScore}`;
@@ -88,21 +85,5 @@ scissorsBtn.addEventListener('click', () => {
     checkForWinner(playerScore, compScore);
 });
 
+
 document.getElementsByClassName('#playerScore').innerText = (playerScore);
-// const game = () => {
-//    for (let i = 0; i < 5; i++) {
-//         const playerSelection = prompt('Choose your weapon', 'Rock, Paper, or Scissors').toLowerCase();
-//         const computerSelection = computerPlay(); 
-//         console.log('1 ', playRound(playerSelection, computerSelection));
-//     } 
-
-//     if (playerScore > compScore) {
-//         return 'Your beat the computer! Way to go!';
-//     } else if (playerScore < compScore) {
-//         return 'Your got beat by the computer! You need to work on your throws.';
-//     } else {
-//         return 'You tied with the computer. Not to shabby!';
-//     }
-// }
-
-// console.log(game());
